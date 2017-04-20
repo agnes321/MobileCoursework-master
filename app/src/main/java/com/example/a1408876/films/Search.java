4ffc1a0ef698a5b5fc1d08aa7db383236df5de76
Agne <agne.skeiryte@gmail.com>
2017-04-20 21:59:15 +0100

    coded on april 20th

--- app/src/main/java/com/example/a1408876/films/Search.java
+++ app/src/main/java/com/example/a1408876/films/Search.java
@@ -1,14 +1,16 @@
 package com.example.a1408876.films;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import java.io.IOException;
-
+import java.util.ArrayList;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Headers;
@@ -17,21 +19,26 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import android.Manifest.permission;
-
+import android.widget.TextView;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
 public class Search extends Fragment {
 
     private SearchView searchField;
+    private LinearLayout ll;
+    private ArrayList<Movie> movies;
+    private Button b;
+    private TextView tv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.search, container, false);
         this.searchField = (SearchView) rootView.findViewById(R.id.searchView);
+        this.ll = (LinearLayout) rootView.findViewById(R.id.linlayId);
 
 
         // perform set on query text listener event
@@ -48,10 +55,10 @@ public class Search extends Fragment {
                     RequestBody body = RequestBody.create(mediaType, "{}");
                     Request request = new Request.Builder().url(url).get().build();
 
-
                             client.newCall(request).enqueue(new Callback() {
                                 @Override public void onFailure(Call call, IOException e) {
                                     e.printStackTrace();
+
                                 }
 
                                 @Override public void onResponse(Call call, Response response) throws IOException {
@@ -61,13 +68,52 @@ public class Search extends Fragment {
                                         System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
                                     }
 
-                                    System.out.println(response.body().string());
+                                    String jsonData  = response.body().string();
+                                    try {
+                                        JSONObject Jobject = new JSONObject(jsonData);
+                                        JSONArray Jarray = Jobject.getJSONArray("results");
+                                        movies = new ArrayList<>(Jarray.length());
+
+                                        for (int i = 0; i < Jarray.length(); i++) {
+                                            JSONObject object = Jarray.getJSONObject(i);
+                                            //System.out.println(object.toString());
+                                            System.out.println(object.getString("title"));
+                                            System.out.println(object.getString("overview"));
+                                            System.out.println(object.getString("release_date"));
+                                            System.out.println(object.getString("original_title"));
+                                            System.out.println(object.getString("original_language"));
+                                            System.out.println(object.getString("popularity"));
+                                            System.out.println(object.getString("vote_average"));
+                                            System.out.println(object.getString("vote_count"));
+                                            System.out.println("-------------");
+
+                                            Movie tempMovie = new Movie();
+                                            tempMovie.setOriginal_title(object.getString("title"));
+                                            tempMovie.setOverview(object.getString("overview"));
+                                            tempMovie.setRelease_date(object.getString("release_date"));
+                                            tempMovie.setOriginal_title(object.getString("original_title"));
+                                            tempMovie.setOriginal_language(object.getString("original_language"));
+                                            tempMovie.setPopularity(object.getString("popularity"));
+                                            tempMovie.setVote_average(object.getString("vote_average"));
+                                            tempMovie.setVote_count(object.getString("vote_count"));
+
+                                            movies.add(tempMovie);
+                                        }
+
+                                    }catch (JSONException e) {
+                                        e.printStackTrace();
+                                        Log.e("MYAPP", "unexpected JSON exception", e);
+
+                                    }
 
 
                                 }
                             });
-
+                    textAppear(tv);
+                    buttonAppear(b);
                 }
+
+
 
                 System.out.println(query);
                 return false;
@@ -76,15 +122,34 @@ public class Search extends Fragment {
             @Override
             public boolean onQueryTextChange(String newText) {
                 // do something when text changes
+                ll.removeAllViews();
                 return false;
             }
         });
+
 
         this.searchField.getQuery();
         return rootView;
     }
 
 
+    public void buttonAppear(Button bt){
 
+        bt = new Button(Search.this.getActivity());
+        bt.setText("Save");
+        ll.addView(bt);
+
+    }
+
+    public void textAppear(TextView rowTextView){
+
+            rowTextView = new TextView(Search.this.getActivity());
+            rowTextView.setText("Hey");
+            rowTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            ll.addView(rowTextView);
+
+    }
 
 }
